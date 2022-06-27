0ffe90fb1ef08cc74a4bb9a348925047fc1031e3
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2022-06-27 22:38:25 +0300

    Entityler oluşturuldu.

---
+++ NLayer.Core/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }//bir kategorinin birden fazla ürünü var
+    }
+}

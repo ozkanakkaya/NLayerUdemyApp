34ab0980f2ca71c9403e39275158aa98ec863b59
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2022-06-28 02:29:34 +0300

    Entity Configurationları yapıldı. (FluentApi)

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -3,6 +3,7 @@ using NLayer.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,5 +19,13 @@ namespace NLayer.Repository
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            //çalışmış olduğumuz assembly i tara ve buradaki IEntityTypeConfiguration interface ine sahip tüm classları bul ve uygula(herbir classlibrary birer assemblydir)
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ NLayer.Repository/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Configurations
+{
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);//Id key olacak.
+            builder.Property(x => x.Id).UseIdentityColumn();//kaçar kaçar artacak(birer birer).
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);//zorunlu,max 50 karakter
+
+            builder.ToTable("Categories");//tablo adı
+
+        }
+    }
+}
